86dc589728b94cca5762bd5c9da42fca1c3a2a6b
Андрей Черёмухин <62459862+AndreyCheruomukhin@users.noreply.github.com>
2023-05-06 03:32:33 +0300

    commit

---
+++ src/js/swiper.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    keyboard: {
+      enabled: true,
+    },
+    navigation: {
+      nextEl: ".button-next",
+      prevEl: ".button-prev",
+    },
+  });
