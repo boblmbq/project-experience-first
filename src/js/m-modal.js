9a3ea79a2615156704a491f52b9a393c8f701731
boblmbq <sichkardanylo@gmail.com>
2023-05-09 22:30:41 +0200

    js made

--- src/js/m-modal.js
+++ src/js/m-modal.js
@@ -2,11 +2,17 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open-mob]'),
     closeModalBtn: document.querySelector('[data-modal-close-mob]'),
+    closeModalLink: document.querySelector('.close-link'),
+    closeModalLi: document.querySelector('.close-link-sale'),
+    closeModalLin: document.querySelector('.close-link-cat'),
     modal: document.querySelector('[data-modal-mob]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalLi.addEventListener('click', toggleModal);
+  refs.closeModalLin.addEventListener('click', toggleModal);
+  refs.closeModalLink.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
